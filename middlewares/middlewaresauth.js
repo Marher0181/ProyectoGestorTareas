6607b8fea22018f839299c73cdf187b55027a810
Marher0181 <marlonher0181@gmail.com>
2024-11-15 13:17:29 -0600

    Endpoints y middleware

---
+++ middlewares/middlewaresauth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const verificarTokenYRol = (requiredRole) => {
+  return (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+      return res.status(403).json({ message: 'Token no proporcionado' });
+    }
+
+    try {
+      const decoded = jwt.verify(token, 'tu_secreto_aqui');
+
+      req.user = decoded;
+
+      if (requiredRole && req.user.rol !== requiredRole) {
+        return res.status(403).json({ message: 'No tienes permisos para realizar esta acción' });
+      }
+
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Token inválido', error: error.message });
+    }
+  };
+};
+
+module.exports = verificarTokenYRol;
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const { employeeModel } = require('../models/employeeModel');
 const { organizationModel } = require('../models/organizationModel');
 const { departmentModel } = require('../models/departmentModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const verificarTokenYRol = require('../middlewares/middlewaresauth');
 
-// Ruta básica para pruebas
 router.post('/', async (req, res) => {
   res.send('Hello world from Employee!');
 });
 
-// Ruta para agregar un nuevo empleado
-router.post('/add', async (req, res) => {
+router.post('/add', verificarTokenYRol('Administrador Dept'), async (req, res) => {
   try {
     const { nombre, rol, codigo, pass, email } = req.body;
 
@@ -19,10 +19,8 @@ router.post('/add', async (req, res) => {
       return res.status(400).json({ message: 'Todos los campos son obligatorios' });
     }
 
-    // Buscar la organización y el departamento basado en el código recibido
-    const orgCodigo = codigo.slice(0, 8); // Primeros 6 caracteres son el código de la organización
-    const deptCodigo = codigo.slice(8, 16);  // Los últimos caracteres son el código del departamento
-    console.log(deptCodigo)
+    const orgCodigo = codigo.slice(0, 8);
+    const deptCodigo = codigo.slice(8, 16);
     const organization = await organizationModel.findOne({ codigo: orgCodigo });
     if (!organization) {
       return res.status(404).json({ message: 'Organización no encontrada' });
@@ -39,6 +37,86 @@ router.post('/add', async (req, res) => {
     const employee = new employeeModel({
       nombre,
       rol,
+      Department: department,
+      pass: hashedPass,
+      email
+    });
+
+    const savedEmployee = await employee.save();
+    return res.status(201).json({ employee: savedEmployee });
+
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ message: 'Error interno del servidor', error: error.message });
+  }
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const { nombre, codigo, pass, email } = req.body;
+
+    if (!nombre || !codigo || !pass || !email) {
+      return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+    }
+
+    const orgCodigo = codigo.slice(0, 8);
+    const deptCodigo = codigo.slice(8, 16);
+    const organization = await organizationModel.findOne({ codigo: orgCodigo });
+    if (!organization) {
+      return res.status(404).json({ message: 'Organización no encontrada' });
+    }
+
+    const department = await departmentModel.findOne({ codigo: deptCodigo});
+    if (!department) {
+      return res.status(404).json({ message: 'Departamento no encontrado en esta organización' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(pass, salt);
+
+    const employee = new employeeModel({
+      nombre,
+      rol: 'Empleado',
+      Department: department,
+      pass: hashedPass,
+      email
+    });
+
+    const savedEmployee = await employee.save();
+    return res.status(201).json({ employee: savedEmployee });
+
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ message: 'Error interno del servidor', error: error.message });
+  }
+});
+
+router.post('/newAdminDept', verificarTokenYRol('Administrador Dept'), async (req, res) => {
+  try {
+    const { nombre, pass, email } = req.body;
+
+    if (!nombre || !pass || !email) {
+      return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+    }
+
+    const orgCodigo = codigo.slice(0, 8);
+    const deptCodigo = codigo.slice(8, 16);
+    const organization = await organizationModel.findOne({ codigo: orgCodigo });
+    if (!organization) {
+      return res.status(404).json({ message: 'Organización no encontrada' });
+    }
+
+    const department = await departmentModel.findOne({ codigo: deptCodigo});
+    if (!department) {
+      return res.status(404).json({ message: 'Departamento no encontrado en esta organización' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(pass, salt);
+
+    const employee = new employeeModel({
+      nombre,
+      rol: 'Empleado',
       Department: department,
       pass: hashedPass,
       email
@@ -73,7 +151,7 @@ router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
 
-    const employee = await employeeModel.findById(id).populate('Department', 'nombre codigo');
+    const employee = await employeeModel.findById(id).populate('Department');
 
     if (!employee) {
       return res.status(404).json({ message: 'Empleado no encontrado' });
@@ -147,6 +225,35 @@ router.delete('/delete/:id', async (req, res) => {
     await employee.remove();
     return res.status(200).json({ message: 'Empleado eliminado correctamente' });
 
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ message: 'Error interno del servidor', error: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, pass } = req.body;
+
+    if (!email || !pass) {
+      return res.status(400).json({ message: 'Email y contraseña son obligatorios' });
+    }
+
+    const employee = await employeeModel.findOne({ email });
+    if (!employee) {
+      return res.status(404).json({ message: 'Empleado no encontrado' });
+    }
+
+    const isMatch = await bcrypt.compare(pass, employee.pass);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Contraseña incorrecta' });
+    }
+
+    const jwt = require('jsonwebtoken');
+    const token = jwt.sign({ id: employee._id, nombre: employee.nombre, rol: employee.rol }, 'tu_secreto_aqui', { expiresIn: '1h' });
+
+    return res.status(200).json({ message: 'Login exitoso', token });
+    
   } catch (error) {
     console.error('Error:', error);
     return res.status(500).json({ message: 'Error interno del servidor', error: error.message });
