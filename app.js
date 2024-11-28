ec3c9791b0f49082d9b504e523e458201a354088
Marher0181 <marlonher0181@gmail.com>
2024-11-28 10:38:34 -0600

    Prubas subir a render

--- app.js
+++ app.js
@@ -12,21 +12,22 @@ const cors = require('cors');
 
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:3000', // Permite solicitudes desde tu cliente React
-  methods: ['GET', 'POST', 'DELETE', 'PUT'], // Métodos HTTP permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-}));
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? '*' : 'http://localhost:3000', // Permite cualquier origen en producción
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-// Resto de tu configuración de rutas y servidor
+app.use(cors(corsOptions));
+
+// Crear el servidor HTTP y el WebSocket
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3000', // Permite WebSocket desde tu cliente React
+    origin: process.env.NODE_ENV === 'production' ? '*' : 'http://localhost:3000', // Permite conexiones WebSocket desde cualquier origen en producción
     methods: ['GET', 'POST', 'PUT'],
   }
 });
-
 
 io.on('connection', (socket) => {
   console.log(`Nuevo cliente conectado: ${socket.id}`);
@@ -36,17 +37,25 @@ io.on('connection', (socket) => {
   });
 });
 
-// Configuración de rutas: 
-// Instrucciones: Para utilizar las rutas
-// se deben utilizar de esta forma
-// "http://localhost:4000/api/v1/<Ruta objetivo>"
-
+// Configuración de rutas
 app.use('/api/v1/organization', organizationRoutes);
 app.use('/api/v1/department', departmentRoutes);
 app.use('/api/v1/employee', employeeRoutes);
 app.use('/api/v1/task', taskRoutes(io));
 
+// Conectar a la base de datos MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://administrador_607:whsLF7P9@practica6.hmwrf.mongodb.net/PracticaNo9?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB conectado'))
+.catch((err) => console.log('Error de conexión:', err));
+
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-  console.log(`Server listen on http://localhost:${PORT}`);
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+  } else {
+    console.log(`Servidor ejecutándose en https://miapp.render.com`);
+  }
 });
